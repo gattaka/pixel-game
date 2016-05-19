ad4543598ab0b12497fb6c6a3635ccab7ca596e2
gattaka <gattakahynca@seznam.cz>
2016-05-19 15:29:26 +0200

    Back to eclipse, transform into TypeScript

---
+++ main.js
@@ -0,0 +1,5 @@
+/// <reference path="exp.ts"/>
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
---
+++ main.ts
@@ -0,0 +1,12 @@
+/// <reference path="exp.ts"/>
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
