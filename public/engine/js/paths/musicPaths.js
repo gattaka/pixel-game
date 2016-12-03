afd688c5e94ddf17fa718f769db47b4b40713512
gattaka <gattakahynca@seznam.cz>
2016-12-04 00:20:57 +0100

    Music

--- public/engine/js/paths/musicPaths.js
+++ public/engine/js/paths/musicPaths.js
@@ -1,15 +1,7 @@
 var Lich;
 (function (Lich) {
     Lich.MUSIC_PATHS = [
-        ["music/Dirt_2.ogg", Lich.MusicKey.MSC_DIRT_THEME_KEY],
-        // pro rychlejší nahrávání (v ostré verzi bude odkomentováno)
-        /*
-        ["music/Building In Progress.ogg", MusicKey.MSC_BUILD_THEME_KEY],
-        ["music/Boss 1.ogg", MusicKey.MSC_BOSS_THEME_KEY],
-        ["music/Fight In Crystals.ogg", MusicKey.MSC_KRYSTAL_THEME_KEY],
-        ["music/Flood.ogg", MusicKey.MSC_FLOOD_THEME_KEY],
-        ["music/Lava.ogg", MusicKey.MSC_LAVA_THEME_KEY]
-        */
+        ["music/dirt.ogg", Lich.MusicKey.MSC_DIRT_THEME_KEY],
         ["music/chickens.ogg", Lich.MusicKey.MSC_CHICKEN_BOSS_THEME_KEY]
     ];
 })(Lich || (Lich = {}));
