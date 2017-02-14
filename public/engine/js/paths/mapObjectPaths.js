bb3cba2fe5a1ab6532c8b0aeb0fa472ada559dc4
gattaka <gattakahynca@seznam.cz>
2017-02-14 22:27:02 +0100

    JS clean

--- public/engine/js/paths/mapObjectPaths.js
+++
@@ -1,4 +0,0 @@
-var Lich;
-(function (Lich) {
-    Lich.MAP_OBJECT_PATHS = [];
-})(Lich || (Lich = {}));
--- public/engine/js/ts/paths/musicPaths.js
+++
@@ -1,7 +0,0 @@
-var Lich;
-(function (Lich) {
-    Lich.MUSIC_PATHS = [
-        ["music/dirt.ogg", Lich.MusicKey.MSC_DIRT_THEME_KEY],
-        ["music/chickens.ogg", Lich.MusicKey.MSC_CHICKEN_BOSS_THEME_KEY]
-    ];
-})(Lich || (Lich = {}));
